91698d605208fdc017b9f723a56bc1376ba66ba2
Dizarimia <lillemor.sunnegard@outlook.com>
2022-12-05 16:06:05 +0000

    hide secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3mw)3)$l@a3zj&w$s67s_pukm%z)@lgk_fx2!y#y119(92yh_d')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
