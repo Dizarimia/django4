aabf6ebc3a332d91f3c17e793d65659f73d768d4
Dizarimia <lillemor.sunnegard@outlook.com>
2023-01-03 20:44:35 +0000

    update import

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from .models import Item
+
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -41,3 +41,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
