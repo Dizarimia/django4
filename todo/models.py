cac11fab13f5ea75fc222e5784f0c4e8e4bbe133
Dizarimia <lillemor.sunnegard@outlook.com>
2023-01-03 20:27:01 +0000

    item?

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from .models import Item
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
